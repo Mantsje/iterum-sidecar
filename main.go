19c75024f9fe641e4cb4725b9eeeba1578fede12
Mark <marktmmrmn@gmail.com>
2020-04-08 12:22:35 +0200

    Rabbitmq

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 	"net"
 	"os"
 	"runtime"
-	"time"
 
 	"github.com/Mantsje/iterum-sidecar/util"
 	"github.com/prometheus/common/log"
+	"github.com/streadway/amqp"
 )
 
 func sendReadiedFiles(socket Socket, conn net.Conn) {
@@ -43,16 +43,45 @@ func main() {
 	util.Ensure(err, "Socket succesfully opened and listening")
 	socket.Start()
 
-	// Message publisher
 	go func() {
-		fileIdx := 0
-		for {
-			time.Sleep(1 * time.Second)
-			fragment := fmt.Sprintf("file%d.txt", fileIdx)
+		fmt.Printf("Started goroutine which should listen to the message queue.\n")
+		conn, err := amqp.Dial(os.Getenv("BROKER_URL"))
+		util.Ensure(err, "Failed to connect to RabbitMQ")
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+		util.Ensure(err, "Failed to open a channel")
+		defer ch.Close()
+
+		q, err := ch.QueueDeclare(
+			os.Getenv("QUEUE"), // name
+			false,              // durable
+			false,              // delete when unused
+			false,              // exclusive
+			false,              // no-wait
+			nil,                // arguments
+		)
+		util.Ensure(err, "Failed to declare a queue")
+
+		msgs, err := ch.Consume(
+			q.Name, // queue
+			"",     // consumer
+			true,   // auto-ack
+			false,  // exclusive
+			false,  // no-local
+			false,  // no-wait
+			nil,    // args
+		)
+		util.Ensure(err, "Failed to register a consumer")
+
+		for message := range msgs {
+			fragment := fmt.Sprintf("%s", message.Body)
+			fmt.Printf("Received a message: %s\n", fragment)
 			fmt.Printf("putting '%v' on channel\n", fragment)
 			socket.Input <- Msg{fragment}
-			fileIdx++
 		}
+		fmt.Printf("Processed all messages...\n")
+
 	}()
 
 	runtime.Goexit()
