4947e20029283cd46d63eb05acbd3fd895915f3c
Mantsje <edserapperloo@hotmail.com>
2020-05-14 15:48:35 +0200

    add Predecessors carry to Local->Remote and Remote->Local transformation

--- store/metadata.go
+++ store/metadata.go
@@ -10,8 +10,11 @@ import (
 
 // toRemoteMetadata converts a local fragment description's metadata into a remote one
 func toRemoteMetadata(localMeta desc.LocalMetadata) (remoteMeta desc.RemoteMetadata) {
-	remoteMeta = desc.RemoteMetadata{FragmentID: localMeta.FragmentID}
-	remoteMeta.Custom = localMeta.Custom
+	remoteMeta = desc.RemoteMetadata{
+		FragmentID:   localMeta.FragmentID,
+		Predecessors: localMeta.Predecessors,
+		Custom:       localMeta.Custom,
+	}
 	if localMeta.OutputChannel != nil {
 		if env.SidecarConfig == nil {
 			log.Fatalf("Cannot convert output channel to remote queue, because sidecar has no config")
@@ -28,7 +31,9 @@ func toRemoteMetadata(localMeta desc.LocalMetadata) (remoteMeta desc.RemoteMetad
 // toLocalMetadata converts a remote fragment description's metadata into a local one
 // It drops any existing target queue info
 func toLocalMetadata(remoteMeta desc.RemoteMetadata) (localMeta desc.LocalMetadata) {
-	localMeta = desc.LocalMetadata{FragmentID: remoteMeta.FragmentID}
-	localMeta.Custom = remoteMeta.Custom
-	return
+	return desc.LocalMetadata{
+		FragmentID:   remoteMeta.FragmentID,
+		Predecessors: remoteMeta.Predecessors,
+		Custom:       remoteMeta.Custom,
+	}
 }
