029706f577c17ac3527941363188891c80b78dfa
Mantsje <edserapperloo@hotmail.com>
2020-06-08 17:26:51 +0200

    add untracked fragments completed tracking and retroactively processing

--- garbage/fragment_collector.go
+++ garbage/fragment_collector.go
@@ -12,17 +12,19 @@ import (
 // It is responsible for clearing the actual data associated with the fragments
 // once they are no longer needed
 type FragmentCollector struct {
-	Track   chan *desc.LocalFragmentDesc
-	Collect chan desc.IterumID
-	backlog map[desc.IterumID]*desc.LocalFragmentDesc
+	Track     chan *desc.LocalFragmentDesc
+	Collect   chan desc.IterumID
+	backlog   map[desc.IterumID]*desc.LocalFragmentDesc
+	untracked []desc.IterumID
 }
 
 // NewFragmentCollector insatiates a FragmentCollector
 func NewFragmentCollector() FragmentCollector {
 	return FragmentCollector{
-		Track:   make(chan *desc.LocalFragmentDesc, 10),
-		Collect: make(chan desc.IterumID, 10),
-		backlog: map[desc.IterumID]*desc.LocalFragmentDesc{},
+		Track:     make(chan *desc.LocalFragmentDesc, 10),
+		Collect:   make(chan desc.IterumID, 10),
+		backlog:   make(map[desc.IterumID]*desc.LocalFragmentDesc),
+		untracked: []desc.IterumID{},
 	}
 }
 
@@ -44,6 +46,7 @@ func (fgb *FragmentCollector) StartBlocking() {
 		// If a message comes in to be tracked it is safed until later
 		case fragDesc, ok := <-fgb.Track:
 			if !ok {
+				log.Infof("ToTrack channel of FragmentCollector was closed")
 				fgb.Track = nil
 				continue
 			}
@@ -51,11 +54,13 @@ func (fgb *FragmentCollector) StartBlocking() {
 		// If a message comes in to be collected it is ready to be deleted from the disk
 		case fragID, ok := <-fgb.Collect:
 			if !ok {
+				log.Infof("CollectGarbage channel of FragmentCollector was closed")
 				fgb.Collect = nil
 				continue
 			}
 			if _, ok := fgb.backlog[fragID]; !ok {
-				log.Warnf("FragmentCollector got a fragmentID that was not tracked: '%v'", fragID)
+				log.Warnf("FragmentCollector got a fragmentID that was not tracked: '%v', if problematic, will result in an error log later", fragID)
+				fgb.untracked = append(fgb.untracked, fragID)
 				continue
 			}
 			go fgb.deleteFromDisk(fgb.backlog[fragID])
@@ -63,7 +68,18 @@ func (fgb *FragmentCollector) StartBlocking() {
 		}
 	}
 	if len(fgb.backlog) != 0 {
-		log.Errorln("FragmenterGarbageCollector.backlog is not empty at end of lifecycle, unremoved files remain")
+		// Remove all untracked ids
+		for _, untrackedID := range fgb.untracked {
+			if _, ok := fgb.backlog[untrackedID]; !ok {
+				log.Errorf("Untracked ID never showed up in FragmentCollector backlog: '%v'", untrackedID)
+			} else {
+				delete(fgb.backlog, untrackedID)
+			}
+		}
+		// If the backlog is still empty, there was an actual problem
+		if len(fgb.backlog) != 0 {
+			log.Errorln("FragmenterGarbageCollector.backlog is not empty at end of lifecycle, unremoved files remain")
+		}
 	}
 	log.Infof("Both input channels are closed, finishing up FragmentCollector")
 }
